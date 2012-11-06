07882922db5187ff82426f706e56ffa3c4c95c57
Dmitri Pervouchine <pervouchine@gmail.com>
2012-11-06 03:29:15 -0800

    =v1.1

---
+++ makefile
@@ -0,0 +1,49 @@
+SAMDIR=~/samtools/
+GCC=gcc
+OPTN=-lz
+
+.PHONY: all
+
+all: bamflag bamflag2
+
+EXPORT = bamflag-1.1
+
+export:
+	mkdir $(EXPORT)/
+	cp -f *.c $(EXPORT)/
+	cp -f *.h $(EXPORT)/
+	cp -f *.sh $(EXPORT)/
+	cp -f README $(EXPORT)/
+	cp -f VERSION $(EXPORT)/
+	cp -f LICENCE $(EXPORT)/
+	cp makefile $(EXPORT)/
+	tar -cf $(EXPORT).tar $(EXPORT)/
+	gzip $(EXPORT).tar
+	rm -f -r $(EXPORT)/
+	mv $(EXPORT).tar.gz ..
+
+$(SAMDIR)libbam.a:
+	# You need to install samtools
+	# Get it by svn:
+	# svn co https://samtools.svn.sourceforge.net/svnroot/samtools/trunk/samtools
+	# enter the dir and type 'make all'
+	# don't forget to update the SAMDIR varibale in this makefile
+	exit 1	
+
+progressbar.o:	progressbar.c progressbar.h
+	gcc -c progressbar.c 
+
+node.o: node.c node.h
+	gcc -c node.c
+
+node2.o: node2.c node2.h
+	gcc -c node2.c
+
+bamflag: bamflag.c progressbar.o $(SAMDIR)libbam.a node.o
+	$(GCC) $(OPTN) -I $(SAMDIR) bamflag.c progressbar.o node.o $(SAMDIR)libbam.a -o bamflag
+
+bamflag2: bamflag2.c progressbar.o $(SAMDIR)libbam.a node2.o
+	$(GCC) $(OPTN) -I $(SAMDIR) bamflag2.c progressbar.o node2.o $(SAMDIR)libbam.a -o bamflag2
+
+clean:
+	rm -f -r progressbar.o node.o node2.o bamflag bamflag2
---
+++ node2.h
@@ -0,0 +1,34 @@
+//	Copyright 2012 Dmitri Pervouchine (dp@crg.eu), Lab Roderic Guigo
+//	Bioinformatics and Genomics Group @ Centre for Genomic Regulation
+//	Parc de Recerca Biomedica: Dr. Aiguader, 88, 08003 Barcelona
+//	
+//	This file is a part of the 'bamflag' package.
+//	'bamflag' package is free software: you can redistribute it and/or modify
+//	it under the terms of the GNU General Public License as published by
+//	the Free Software Foundation, either version 3 of the License, or
+//	(at your option) any later version.
+//	
+//	'bamflag' package is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details.
+//	
+//	You should have received a copy of the GNU General Public License
+//	along with 'bamflag' package.  If not, see <http://www.gnu.org/licenses/>.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct elem {
+    char character;
+    int  count;
+    struct elem* next;
+    struct elem* tree;
+};
+
+typedef struct elem node_type;
+ 
+void add_str(node_type **node, char *str);
+void retrieve(node_type *node, char *pref);
+int  recall_count(node_type *node, char *str);
+void destroy(node_type *node);
